3cb8535b54dbe357609a1bf38b3552f7683d9346
Ilia Tobov <ilia_tobov@mai.bg>
2014-02-15 00:14:22 +0200

    Display turn, score and winner

--- Reversi/GUI.rb
+++ Reversi/GUI.rb
@@ -11,6 +11,9 @@ SPACE = 50
 X_OFFSET = (WINDOWWIDTH-(WIDTH*SPACE))/2
 Y_OFFSET = (WINDOWHEIGHT-(HEIGHT*SPACE))/2
 
+BLACK_TILE = Reversi::BLACK_CELL
+WHITE_TILE = Reversi::WHITE_CELL
+
 def draw_board
   fill green
   rect X_OFFSET, Y_OFFSET, SPACE*WIDTH, SPACE*HEIGHT
@@ -21,7 +24,7 @@ def draw_board
   end
 end
 
-def get_center(x, y)
+def get_center_pixels(x, y)
   [(X_OFFSET + x*SPACE)+5, (Y_OFFSET + y*SPACE)+5]
 end
 
@@ -30,18 +33,18 @@ def draw_tiles(board)
     (0..WIDTH.pred).each do |x|
       if board[x, y] != 0
         fill TILECOLORS[board[x, y]]
-        oval *get_center(x, y), (SPACE/2)-5
+        oval *get_center_pixels(x, y), (SPACE/2)-5
       end
     end
   end
 end
 
-
-
 def clicked_square_coordinates(left, top)
   (0..HEIGHT.pred).each do |y|
     (0..WIDTH.pred).each do |x|
-      return [x, y] if top > x*SPACE+X_OFFSET and top < (x+1)*SPACE + X_OFFSET and left > y*SPACE + Y_OFFSET and left < (y+1)*SPACE+Y_OFFSET
+      horizontal = top.between?(x*SPACE+X_OFFSET, (x+1)*SPACE+X_OFFSET)
+      vertical = left.between?(y*SPACE+Y_OFFSET, (y+1)*SPACE+Y_OFFSET)
+      return [x, y] if horizontal and vertical
     end
   end
   nil
@@ -59,23 +62,38 @@ def clicked(board)
     board[*coords] = 1
   end
 end
+
 def player_move(coordinates)
   player.make_move(coordinates) ? true : false
 end
 
+def score_and_turn(players, turn)
+  @score.replace "Player one #{players[0].score}, Player two #{players[1].score}"
+  @turn.replace "Turn: #{turn}"
+end
+
+def check_for_winner(board, players)
+  board.positions.all? { |x, y| not board.empty? x, y }
+end
+
+
+
 Shoes.app width: WINDOWWIDTH, height: WINDOWHEIGHT, title: "Reversi" do
-  TILECOLORS = {1 => black, 2 => white}
-  TURNS = {1 => 2, 2=> 1}
-  @move = para
+  TILECOLORS = {BLACK_TILE => black, WHITE_TILE => white}
+  TURNS = {BLACK_TILE => WHITE_TILE, WHITE_TILE => BLACK_TILE}
   background red
   board = Board.new
-  players = [Human.new(board, 1), Human.new(board, 2)]
-  # player_one = Human.new board, 1
-  # player_two = Human.new board, 2
+  players = [Human.new(board, BLACK_TILE), Human.new(board, WHITE_TILE)]
   draw_board
   draw_tiles board
-  turn = 1
-  @move.replace "#{turn}"
+  turn = BLACK_TILE
+
+  @score = para
+  @turn = para
+  @winner = para
+
+  score_and_turn players, turn
+
   click do |button, top, left|
     if button == 1
       coords = clicked_square_coordinates left, top
@@ -85,10 +103,12 @@ Shoes.app width: WINDOWWIDTH, height: WINDOWHEIGHT, title: "Reversi" do
       move = player_to_make_move.make_move coords
       if move
         turn = TURNS[turn] unless other_player.valid_moves.empty?
-        @move.replace "#{turn}"
+        score_and_turn players, turn
         draw_board
         draw_tiles board
       end
+
+      @winner.replace "winner winner chicken dinner" if check_for_winner board, players
     end
   end
 end
