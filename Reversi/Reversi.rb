e82172cbae2a62f39ebd740fe1992d17ff6be79b
Ilia Tobov <ilia_tobov@mai.bg>
2014-02-16 00:52:16 +0200

    Computer can be medium or hard
    Also add Positions module

--- Reversi/Reversi.rb
+++ Reversi/Reversi.rb
@@ -5,7 +5,7 @@ module Reversi
   BLACK_CELL = 1
   WHITE_CELL = 2
   DIRECTIONS = [[0, 1], [1,1], [1, 0], [1, -1],
-              [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+              [0, -1], [-1, -1], [-1, 0], [-1, 1]].freeze
   OTHER_PLAYER = {BLACK_CELL => WHITE_CELL, WHITE_CELL => BLACK_CELL}
 
 
@@ -107,136 +107,81 @@ module Reversi
   end
 
   class Computer < Player
-    def easy(cell=nil)
+    def make_move(difficulty)
+      move = send difficulty
+      tiles_to_flip(move).each { |cell| board[*cell] = tile }
+    end
+
+    def easy
+      index = Random.rand(valid_moves.length)
+      valid_moves[index]
+    end
+
+    def medium
+      possible_moves = valid_moves.shuffle
+      index = possible_moves.map { |position| tiles_to_flip(position).size }.each_with_index.max[1]
+      possible_moves[index]
+    end
+
+    def hard
+      possible_moves = good_moves
+      if possible_moves.empty?
+        possible_moves = bad_moves
+      end
+
+      index = possible_moves.map { |position| tiles_to_flip(position).size }.each_with_index.max[1]
+      possible_moves[index]
+    end
+
+    def bad_moves
       possible_moves = valid_moves
-      index = Random.rand(possible_moves.length)
-      tiles_to_flip(possible_moves[index]).each { |move| board[*move] = tile }
+
+      risk_moves = possible_moves.select { |move| Positions::RISK.include? move}
+      really_bad_moves = possible_moves.select { |move| Positions::BAD.include? move}
+
+      not_so_bad_moves = possible_moves - risk_moves - really_bad_moves
+
+      if not_so_bad_moves.any?
+        not_so_bad_moves
+      elsif risk_moves.any?
+        risk_moves
+      else
+        really_bad_moves
+      end
     end
-  end
-end
 
-class Game
-  attr_accessor :board, :player_one, :player_two, :gui
+    def good_moves
+      possible_moves = valid_moves
 
-  def initialize(width = 8, height = 8)
-    @board = Reversi::Board.new
-    @player_one = Reversi::Human.new @board, Reversi::BLACK_CELL
-    @player_two = Reversi::Computer.new @board, Reversi::WHITE_CELL
-    @gui = Console_GUI.new
-    @turn = Reversi::BLACK_CELL
-  end
+      corner_moves = possible_moves.select { |move| Positions::CORNERS.include? move}
+      edge_moves = possible_moves.select { |move| Positions::EDGES.include? move}
 
-  def run_game
-    player, other_player = nil, nil
-    difficulty = gui.difficulty if player_two.is_a? Reversi::Computer
-
-    while true
-      gui.tiles board.cells.values
-      puts gui
-      gui.score player_one.score, player_two.score
-
-      player, other_player = player_one.tile == @turn ? [player_one, player_two] : [player_two, player_one]
-
-      break if player.valid_moves.empty?
-
-      gui.turn player.tile
-      player.is_a?(Reversi::Human) ? player_move(player) : computer_move(player, difficulty)
-
-      if not other_player.valid_moves.empty?
-        @turn = other_player.tile
+      if corner_moves.any?
+        corner_moves
+      else
+        edge_moves
       end
     end
   end
 
-  def player_move(player)
-    move = nil
+  module Positions
+    CORNERS = [[0, 0], [WIDTH-1, 0], [0, HEIGHT-1], [WIDTH-1, HEIGHT-1]].freeze
 
-    until move
-      move = gui.player_input
+    vertical_edges = [0, WIDTH-1].product 2.upto(HEIGHT-3).to_a
+    horizontal_edges = 2.upto(WIDTH-3).to_a.product [0, HEIGHT-1]
 
-      move.map! { |position| position - 1 }
-      if not move.nil? and not player.is_valid_move? move
-        gui.invalid_move
-        move = nil
-      end
-    end
+    EDGES = (vertical_edges + horizontal_edges).freeze
 
-    gui.tiles player.board.cells.values
-    player.make_move move
-  end
+    vertical_risk_positions = [1, WIDTH-2].product 2.upto(HEIGHT-3).to_a
+    horizontal_risk_positions = 2.upto(WIDTH-3).to_a.product [1, HEIGHT-1]
 
-  def computer_move(computer, difficulty)
-    gui.tiles computer.board.values
-    computer.send difficulty
+    RISK = (vertical_risk_positions + horizontal_risk_positions).freeze
+
+    BAD = [[1, 0], [1, 1], [0, 1], [WIDTH-2, 0], [WIDTH-1, 1],
+               [WIDTH-2, 1], [0, HEIGHT-2], [HEIGHT-1, 1], [HEIGHT-2, 1],
+               [WIDTH-2, HEIGHT-1], [WIDTH-2, HEIGHT-2], [WIDTH-1, HEIGHT-2]].freeze
   end
 end
 
-class Console_GUI
-  POSITION = "| %s ".freeze
-  ROW = "----".freeze
-  EDGE = "  " + ROW*8 + "-\n".freeze
-  ROW_NUMBER = "%d ".freeze
-  CELLS = {1 => "B".freeze, 2 => "W".freeze, 0 => " ".freeze}
-  TURN = {1 => "\nPlayer one's turn\n".freeze, 2 => "\nPlayer two's turn\n".freeze}
-  DIFFICULTIES = ["easy", "medium", "hard"]
-
-  attr_accessor :boardGUI, :tiles
-
-  def initialize
-    @boardGUI = ""
-    @tiles = []
-  end
-
-  def score(player_one, player_two)
-    puts "\nPlayer 1: %s   Player 2: %s\n" % [player_one, player_two]
-  end
-
-  def to_s
-    "\n" + boardGUI % @tiles + "\n"
-  end
-
-  def invalid_move
-    puts "\n\n-------------\nInvalid move!\n-------------\n\n"
-  end
-
-  def tiles(values)
-    @tiles = []
-    values.each { |value| @tiles << CELLS[value] }
-  end
-
-  def boardGUI
-    @boardGUI = "    " + 1.upto(8).map(&:to_s).join("   ") + "\n"
-    (1..8).each do |i|
-      @boardGUI += EDGE + ROW_NUMBER % i.to_s + POSITION*8 + "|\n"
-    end
-    @boardGUI += EDGE
-  end
-
-  def player_input
-    puts "\nMake your move! It should be two digits separated by space\n"
-    input = nil
-    until input
-      input = /^[1-8] [1-8]$/.match gets.chomp
-      invalid_move if input.nil?
-    end
-    input.to_s.split(' ').map(&:to_i)
-  end
-
-  def turn(tile)
-    puts TURN[tile]
-  end
-
-  def difficulty
-    puts "\nChoose computer's difficulty: easy, medium or hard\n"
-    difficulty = nil
-    until difficulty
-      difficulty = gets.chomp
-      difficulty = nil unless DIFFICULTIES.include?(difficulty)
-    end
-    difficulty
-  end
-end
-
-
-game = Game.new
-game.run_game
+# board = Reversi::Board.new
+# players = [Reversi::Human.new(board, 1), Reversi::Human.new(board, 2)]
